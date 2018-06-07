6924e66e0836853289baacf2768fdfced062a388
Adem-D <38308837+Adem-D@users.noreply.github.com>
2018-06-07 15:59:23 +1000

    Update moveTo.c
    Working moveTo using setForces and setPosition

--- moveTo.c
+++ moveTo.c
@@ -1,6 +1,25 @@
-void moveTo(float pos[3]) {
-  // Basic barebones algorithm. Movement group should improve upon it by using setVelocity or setForces
-  api.setPositionTarget(pos);
+// debugs can be removed
 
+void moveTo(float positionTarget[3]){
+    float myState[12];
+    float myPos[3];
+    float vectorBetween[3];
+    float distance;
+    api.getMyZRState(myState);
+    for (int i = 0; i<3; i++)
+        myPos[i] = myState[i];
+
+    mathVecSubtract(vectorBetween, positionTarget, myPos, 3);
+    distance = mathVecMagnitude(vectorBetween, 3);
+    
+  
+    //distance can be adjusted to maximise fuel usage and speed
+    if (distance>0.2){
+        api.setForces(vectorBetween);
+        DEBUG(( "Using setForces"));
+    }
+    else{
+        api.setPositionTarget(positionTarget);
+        DEBUG(( "Using setPosition"));
+    }
 }
-
